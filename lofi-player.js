4834401dc1d1ca72bf0ac13c3d8a35bf75cfec8c
Gabrielle Charlton <46015102+gabcharlton@users.noreply.github.com>
2024-12-03 21:50:49 -0800

    Update lofi-player.js

--- lofi-player.js
+++ lofi-player.js
@@ -9,8 +9,8 @@ const lofiPlayer = document.getElementById('lofi-player');
 // Tracks
 const tracks = [
   'audio/lofi_breno-aesthetic_groove _thematic.mp3',
-  'audio/lofi_breno-tape-vibes.mp3',
-  'audio/lofi_breno-timed.mp3'
+  'audio/lofi_breno-tape_vibes_thematic.mp3',
+  'audio/lofi_breno-timed_thematic.mp3.mp3'
 ];
 let currentTrack = 0;
 
